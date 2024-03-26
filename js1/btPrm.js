edc32999ec9cf8ae71df6d52b1385a37ad356e5c
hoangvtiendat2004 <161110799+hoangvtiendat2004@users.noreply.github.com>
2024-03-26 22:11:02 +0700

    crud

--- js1/btPrm.js
+++ js1/btPrm.js
@@ -1,71 +1,65 @@
-const data  = fetch("https://jsonplaceholder.org/posts?fbclid=IwAR1ZQ3Mh7TaHy03yb_-4Yl4_RlmR1YEuTYLqn8BGThT7xg4YjGrmEkE5uTo")
+const data = fetch("https://jsonplaceholder.org/posts?fbclid=IwAR1ZQ3Mh7TaHy03yb_-4Yl4_RlmR1YEuTYLqn8BGThT7xg4YjGrmEkE5uTo")
 
-data.then(function(res)
-{
+data.then(function (res) {
     console.log(res)
     return res.json()
 })
-.then(function(res)
-{
-    console.log(res)
-    const child = res.map(function(item)
-    {
-        return `
-        <div class="article">
-                    <div class = "img">
-                        <img class="image" src="${item.image}"
-                        alt="">
-                        <img class="thumbnail" src="${item.thumbnail}" alt="">
-                        <div class = "status">
-                            <div class="tltSts">${item.status}</div>
-                        </div>
-                        <div class="id">${item.id}</div>
+    .then(function (res) {
+        console.log(res)
+        const child = res.map(function (item) {
+            return `
+    <div class="article">
+                <div class = "img">
+                    <img class="image" src="${item.image}"
+                    alt="">
+                    <img class="thumbnail" src="${item.thumbnail}" alt="">
+                    <div class = "status">
+                        <div class="tltSts">${item.status}</div>
+                    </div>
+                    <div class="id">${item.id}</div>
 
+
+                </div>
+                
+                <div class = "context">
+                    <div class="title">${item.title}</div>
+                    <div class="ct">
+                        <div class="content">${item.content}</div>
+                
+                    </div>
+
+
+                    <a href="${item.image}" class="link">link url</a>
+
+                    <div class="category">
+                        <div class="titlect">slug:</div>
+                        <div class="contentct">${item.slug}</div>
 
                     </div>
-                    
-                    <div class = "context">
-                        <div class="title">${item.title}</div>
-                        <div class="ct">
-                            <div class="content">${item.content}</div>
-                    
-                        </div>
-
-
-                        <a href="${item.image}" class="link">link url</a>
-
-                        <div class="category">
-                            <div class="titlect">slug:</div>
-                            <div class="contentct">${item.slug}</div>
-
-                        </div>
-                        <div class="category">
-                            <div class="titlect">category: </div>
-                            <div class="contentct">${item.category}</div>
-                        </div>
-                        <div class="publishedAt">
-                            <div class="pubTlt">published At: </div>
-                            <div class="pubct">${item.publishedAt}</div>
-                        </div>
-                        <div class="publishedAt">
-                            <div class="pubTlt">updated At: </div>
-                            <div class="pubct">${item.updatedAt}</div>
-                        </div>
+                    <div class="category">
+                        <div class="titlect">category: </div>
+                        <div class="contentct">${item.category}</div>
                     </div>
-                    
+                    <div class="publishedAt">
+                        <div class="pubTlt">published At: </div>
+                        <div class="pubct">${item.publishedAt}</div>
+                    </div>
+                    <div class="publishedAt">
+                        <div class="pubTlt">updated At: </div>
+                        <div class="pubct">${item.updatedAt}</div>
+                    </div>
+                </div>
+                
 
-        </div>`
+    </div>`
+        })
+        let container = document.getElementById("container")
+        container.innerHTML = child.join("")
+
     })
-    let container = document.getElementById("container")
-    container.innerHTML = child.join("")
-   
-})
-.catch(function(err)    
-{
-    console.log("Error: ",err)
-})
-.finally(function()
-{
-    console.log("finally")
-})
-
+    .catch(function (err) {
+        console.log("Error: ", err)
+    })
+    .finally(function () {
+        console.log("finally")
+    })
