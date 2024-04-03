50f0bb38c518023fd40d4d449e0d616089a6ae68
hoangvtiendat2004 <161110799+hoangvtiendat2004@users.noreply.github.com>
2024-04-03 13:10:27 +0700

    update

--- TodoList/loginS.js
+++ TodoList/loginS.js
@@ -6,30 +6,38 @@ function checkValid() {
     console.log(email);
     console.log(password);
 
-    var checkus = /^(?=.*[a-z])(?=.*\d)[a-zA-Z\d]+$/
-    var checkpw = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]+$/
+    var checkus = /^(?=.*[a-z])(?=.*[!@#$%^&*._ ])[a-z!@#$%^&*._]+$/
+    var checkpw = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*_])[a-zA-Z\d!@_#$%^&*]+$/
 
-    var check = true
+    var check = 0
     if (!checkus.test(email)) {
         console.log("username error")
-        check = false
+        check += 1;
         document.querySelector(".errEmail").style.display = "inline-block";
 
+    }
+    else {
+        console.log("username ok")
 
-
+        document.querySelector(".errEmail").style.display = "none";
     }
     if (!checkpw.test(password)) {
         console.log("password error")
-        check = false
+        check += 1
         document.querySelector(".errPassword").style.display = "inline-block";
     }
-    if (check == false) {
+    else {
+        console.log("password ok")
+
+        document.querySelector(".errPassword").style.display = "none";
+    }
+    if (check != 0) {
         return false
 
     }
-    else if (check == true) {
-        document.querySelector(".errPassword").style.display = "none";
-        document.querySelector(".errEmail").style.display = "none";
+    else {
+        document.querySelector(".errPassword").style.display = "";
+        document.querySelector(".errEmail").style.display = "";
         return true;
     }
 
@@ -37,11 +45,49 @@ function checkValid() {
 }
 
 
-function login()
-{
-    if(checkValid() === true)
-    {
-        console.log("ok")
+
+function login() {
+
+    if (checkValid() === true) {
+        const formData = {
+           login : email.value,
+           password : password.value
+        }
+       
+        // console.log( "email: ", us, "\npw: ", pw);
+        const post = fetch("https://recruitment-api.pyt1.stg.jmr.pl/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+
+        post.then(reponsive => {
+            if (reponsive.ok) {
+                return reponsive.json(); // Chuyển đổi phản hồi JSON thành đối tượng JavaScript
+            } else {
+                throw new Error('Network response was not ok');
+                
+            }
+        }).then(data => {
+            if (data.status === 'ok') {
+                window.location.href = 'todoList.html';
+            } else {
+                document.querySelector(".errPassword").style.display = "inline-block";
+                document.querySelector(".errEmail").style.display = "inline-block";
+              
+            }
+        }).catch(er=>{
+            console.log("error:" , er);
+        }).finally(function()
+        {
+            console.log("done")
+        })
+
     }
 
+
 }
+
